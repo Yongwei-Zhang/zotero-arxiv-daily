2501085d772827b6c65f064f219956c035f510d1
Yongwei Zhang <zhangyongwei@buaa.edu.cn>
2026-04-10 10:29:38 +0800

    Refactor SentenceTransformer initialization
    Removed trust_remote_code parameter from SentenceTransformer initialization as it is now included by default in the latest version.

--- src/zotero_arxiv_daily/reranker/local.py
+++ src/zotero_arxiv_daily/reranker/local.py
@@ -19,8 +19,6 @@ class LocalReranker(BaseReranker):
             logging.getLogger("huggingface_hub.utils._http").setLevel(logging.ERROR)
             warnings.filterwarnings("ignore", category=FutureWarning)
 
-        # transformers 新版本中，已经默认包含了 trust_remote_code 这个参数，因此将其删去
-        # encoder = SentenceTransformer(self.config.reranker.local.model, trust_remote_code=True)
         encoder = SentenceTransformer(self.config.reranker.local.model)
         if self.config.reranker.local.encode_kwargs:
             encode_kwargs = self.config.reranker.local.encode_kwargs
